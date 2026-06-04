3fd9edc940f274f97b92421b3b260d4d1279cc29
Rahul Gupta <dreamer07rahul@gmail.com>
2026-06-04 16:54:36 +0530

    Added

---
+++ Day-3/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+int main() {
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    while (b != 0){
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    printf("GCD is: %d", a);
+    return 0;
+}
