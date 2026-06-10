223ff37ed9abf2ea5312a6f25aa15b4af4a15017
Rahul Gupta <dreamer07rahul@gmail.com>
2026-06-10 20:24:50 +0530

    added

---
+++ Day-10/Character Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int i,j;
+    char ch;
+    int n=5;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        ch='A';
+        for(j=1;j<=i;j++){
+            printf("%c",ch);
+            ch++;
+        }
+        ch-=2;
+        for(j=1;j<i;j++){
+            printf("%c",ch);
+            ch--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
